d8789b2dbcf2e5f2a5912c69ba05752fab84c075
Fly <tony91097@gmail.com>
2020-06-13 23:15:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/splashdemo/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.splashdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        
+    }
+
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
